c3d48783d6854083c7ab7ba1bde184d42980a78e
Arie <s7925al@gmail.com>
2025-05-27 16:45:40 +0300

    1

---
+++ Enemy_factory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Commandos.models
+{
+    public class Enemy_factory
+    {
+    static List<Enemies> Listenemies = new List<Enemies>();
+        
+
+        public static void Factory(string name)
+        {
+            Listenemies.Add(new Enemies(name));
+        }
+       
+        
+    
+    }
+}
